fff18a5a303365734a4dbacba3289f3648f91b69
mashige.rudzani <mashige.rudzani@gmail.com>
2025-03-15 20:11:54 +0200

    fixing somethings

--- main.tf
+++ main.tf
@@ -114,8 +114,8 @@ resource "aws_eks_addon" "ebs-csi" {
 
 provider "helm" {
   kubernetes {
-    host                   = eks.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    host                   = module.eks.cluster-endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.main.token
   }
 }
