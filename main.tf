727d6ab3afc9c720163729530f3d7ffa657f86fd
mshrud002 <42965772+mshrud002@users.noreply.github.com>
2025-03-16 09:41:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -177,7 +177,8 @@ resource "aws_lb_target_group" "rancher_target_group" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
-  target_type = "ip"
+  #target_type = "IP"
+  target_type = "instance" 
 
   health_check {
     path                = "/healthz"
