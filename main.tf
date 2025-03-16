e1d3db2800e06553e3205bd7ac063c6a13ce6794
mshrud002 <42965772+mshrud002@users.noreply.github.com>
2025-03-16 20:36:45 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,6 +123,13 @@ resource "aws_eks_addon" "ebs-csi" {
 }
 
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
+
+
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
